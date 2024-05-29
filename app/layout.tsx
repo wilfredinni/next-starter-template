0aea79b824a43411321e24b7a95e0172b6574cab
Eranda Sanjaya <76166067+ES-3508@users.noreply.github.com>
2024-05-29 15:47:57 +0530

    Sample album

--- app/layout.tsx
+++ app/layout.tsx
@@ -83,10 +83,10 @@ export default function RootLayout({ children }: Props) {
                       </Link>
                       <Link
                         className="flex items-center gap-3 rounded-lg px-3 py-2 text-gray-500 transition-all hover:text-gray-900 dark:text-gray-400 dark:hover:text-gray-50"
-                        href="#"
+                        href="user"
                       >
                         <PackageIcon className="h-4 w-4" />
-                        Products
+                        Users
                       </Link>
                       <Link
                         className="flex items-center gap-3 rounded-lg px-3 py-2 text-gray-500 transition-all hover:text-gray-900 dark:text-gray-400 dark:hover:text-gray-50"
--- app/user/page.tsx
+++ app/user/page.tsx
@@ -1,10 +1,11 @@
 "use client";
 
+import Users from "@/components/users";
 
 export default function Page() {
   return (
     <main className="flex flex-1 flex-col gap-4 p-4 md:gap-8 md:p-6">
-      Home page content
+      <Users />
     </main>
   );
 }
